af46b535cbdddcefb6dfc5ece720814297dfc097
Michael Schams <typo3@2014.trash.schams.net>
2014-06-22 17:05:07 +1000

    [TASK] Allow to limit the number of files to process. [TASK] Documentation updated

--- README.md
+++ README.md
@@ -5,6 +5,9 @@ Objective
 ---------
 
 This bash script makes a mass-update on all .php files of a TYPO3 CMS source directory and replaces the existing copyright header with a new one.
+It is possible to limit the number of files to process to a specific value (default: 100).
+
+The script was used for the re-working and simplifing the copyright header in TYPO3 CMS' PHP files, see: [issue 59535](http://forge.typo3.org/issues/59535) on forge.typo3.org.
 
 Installation
 ------------
@@ -21,18 +24,21 @@ Change into the `copyright` directory and execute the script `copyright.sh`:
 
 ```
 cd copyright
-./copyright.sh typo3_src.git
+./copyright.sh typo3_src.git 100
 ```
 
 This will search for *.php files in `typo3_src.git` and replace the copyright header with the content of file `copyright-php-files.txt`.
+It stops after processing 100 files and creates a list of already processed files as file `copyright.exclude` (in order to prevent processing these files again).
 
 Features And Considerations
 ---------------------------
 
 - The script creates a file `copyright.ignored` which contains a list of files that were ignored during the last run (e.g. due to missing copyright headers)
+- The script creates a file `copyright.exclude` which contains a list of files that were already processed
 - The script excludes directories `.git` and `contrib` during the scan
 - Copyright headers in *.php files must occur in the first 42 lines (see script source)
-- Copyright headers must contain at least the words "copyright" and "typo3"
+- Copyright headers must contain at least the words "copyright" and "typo3" (case insensitive)
+- The script writes MD5 hashes of old headers in directory `/tmp/typo3header/` to detect unique headers found
 
 Author
 ------
--- copyright.sh
+++ copyright.sh
@@ -13,15 +13,27 @@
 INCLUDE_FILE_PATTERN='\.php$'
 
 DIRECTORY=$1
+PROCESS_MAX=$2
 
 if [ "${DIRECTORY}" = "" ]; then
-	echo -e "\nUsage: $0 <typo3_src directory>\n"
+	echo -e "\nUsage: $0 <typo3_src directory> [<max number of files to process>]\n"
 	exit 1
 fi
 
 if [ ! -d "${DIRECTORY}" ]; then
 	echo -e "\nDirectory \"${DIRECTORY}\" does not exist\n"
 	exit 1
+fi
+
+TEMP=$(echo "${PROCESS_MAX}" | egrep '^[0-9]{1,}$')
+if [ "${TEMP}" = "" ]; then
+	PROCESS_MAX=100
+fi
+
+if [ -d "/tmp/typo3headers" ] ; then
+	find "/tmp/typo3headers" -type f -exec rm {} \;
+else
+	mkdir "/tmp/typo3headers"
 fi
 
 if [ -e "copyright.ignored" ]; then
@@ -31,20 +43,22 @@ fi
 COUNTER=0
 PROCESSED=0
 SKIPPED=0
+ALREADY_PROCESSED=0
+UNIQUE_HEADERS=0
 
 DIRECTORY=$(echo "${DIRECTORY}" | sed 's/\/$//g')
-
 TIME_START=$(date +"%s")
+touch "copyright.exclude"
 
+#echo ; echo "Maximum number of files to process: ${PROCESS_MAX}" ; echo
 echo ; echo "Scanning directory..."
 FILELIST=$(find "${DIRECTORY}" -path "${DIRECTORY}/.git" -path "${DIRECTORY}/contrib" -prune -o -type f -print | egrep "${INCLUDE_FILE_PATTERN}")
-
 TOTAL_FILES=$(echo "${FILELIST}" | wc --line)
 
 tput cuu1
 echo "Scanning directory......: ${TOTAL_FILES} files found"
 
-echo -e "\n\n\n"
+echo -e "\n\n\n\n\n"
 
 declare -i START_LINENUMBER
 declare -i END_LINENUMBER
@@ -73,35 +87,79 @@ for FILE in $(echo "${FILELIST}") ; do
 
 		if [ ! "${START_LINENUMBER}" = "" -a ! "${END_LINENUMBER}" = "" -a ${START_LINENUMBER} -gt 0 -a ${END_LINENUMBER} -gt 0 -a ${END_LINENUMBER} -lt ${TOTAL_LINES} ]; then
 
-			let START_LINENUMBER=${START_LINENUMBER}-1
-			let END_LINENUMBER=${END_LINENUMBER}+1
+			let START_LINENUMBER=${START_LINENUMBER}
+			let END_LINENUMBER=${END_LINENUMBER}
 			let TOTAL_LINES=${TOTAL_LINES}+1
 
-			cat "${FILE}" | head -${START_LINENUMBER} > "${FILE}.copyright"
-			cat "copyright-php-files.txt" >> "${FILE}.copyright"
-			cat "${FILE}" | sed -n "${END_LINENUMBER},${TOTAL_LINES}p" >> "${FILE}.copyright"
-			mv "${FILE}.copyright" "${FILE}"
+			# determine MD5 hash of path/filename
+			FILENAME_MD5=$(echo "${FILE}" | md5sum | cut -f 1 -d ' ')
 
-			let PROCESSED=${PROCESSED}+1
+			EXCLUDE_FROM_PROCESS="false"
+			TEMP=$(cat "copyright.exclude" | grep "${FILENAME_MD5}")
+			if [ ! "${TEMP}" = "" ]; then
+				EXCLUDE_FROM_PROCESS="true"
+			fi
+
+			# extract header
+			OLD_HEADER=$(cat "${FILE}" | sed -n "${START_LINENUMBER},${END_LINENUMBER}p")
+			OLD_HEADER_MD5=$(echo "${OLD_HEADER}" | md5sum | cut -f 1 -d ' ')
+			if [ ! -e "/tmp/typo3headers/${OLD_HEADER_MD5}" ] ; then
+				echo "${OLD_HEADER}" > "/tmp/typo3headers/${OLD_HEADER_MD5}"
+				let UNIQUE_HEADERS=UNIQUE_HEADERS+1
+			fi
+
+			if [ "${OLD_HEADER_MD5}" = "fbe60edac6cc33f523fb4ecffa055cb4" -o "${EXCLUDE_FROM_PROCESS}" = "true" ]; then
+				# file already shows new header or is excluded from process
+				echo "[A] ${FILE}" >> "copyright.ignored"
+				let SKIPPED=${SKIPPED}+1
+				let ALREADY_PROCESSED=${ALREADY_PROCESSED}+1
+			else
+				# first part
+				let LINENUMBER=${START_LINENUMBER}-1
+				cat "${FILE}" | head -${LINENUMBER} > "${FILE}.copyright"
+
+				# second part (copyright header)
+				cat "copyright-php-files.txt" >> "${FILE}.copyright"
+
+				# third part (rest of the file)
+				let LINENUMBER=${END_LINENUMBER}+1
+				cat "${FILE}" | sed -n "${LINENUMBER},${TOTAL_LINES}p" >> "${FILE}.copyright"
+
+				mv "${FILE}.copyright" "${FILE}"
+
+				echo "${FILENAME_MD5}" >> "copyright.exclude"
+
+				let PROCESSED=${PROCESSED}+1
+			fi
 		else
+			# unable to determine start/end line numbers
 			echo "[L] ${FILE}" >> "copyright.ignored"
 			let SKIPPED=${SKIPPED}+1
 		fi
 	else
+		# something suspecious with the current header (maybe no header exists?)
 		echo "[H] ${FILE}" >> "copyright.ignored"
 		let SKIPPED=${SKIPPED}+1
 	fi
 
 	let TIME_ELAPSED=$(date +"%s")-${TIME_START}
 
-	tput cuu1 ; tput cuu1 ; tput cuu1 ; tput cuu1
+	tput cuu1 ; tput cuu1 ; tput cuu1 ; tput cuu1 ; tput cuu1 ; tput cuu1
 	echo "Files checked...........: ${COUNTER} of ${TOTAL_FILES}"
-	echo "Files processed.........: ${PROCESSED}"
+	echo "Files processed.........: ${PROCESSED} of ${PROCESS_MAX}"
 	echo "Files skipped...........: ${SKIPPED}"
+	echo "Files already processed.: ${ALREADY_PROCESSED}"
+	echo "Unique headers found....: ${UNIQUE_HEADERS}"
 	echo "Time elapsed (seconds)..: ${TIME_ELAPSED}"
+
+	if [ ${PROCESSED} -ge ${PROCESS_MAX} ]; then
+		echo
+		echo "Maximum allowed number of files to process reached/exceeded."
+		break
+	fi
+
 done
 
 echo
 
 exit 0
-
